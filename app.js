ea9065c7ee8e8236dab6bb98b270ec62ccf4d26c
Forth <33458877+Forthtilliath@users.noreply.github.com>
2022-09-24 21:48:03 +0200

    js-custom-select

---
+++ app.js
@@ -0,0 +1,83 @@
+// https://www.w3schools.com/howto/howto_custom_select.asp
+
+const customSelects = document.querySelectorAll(".custom-select");
+
+/* This is the main function that is creating the custom select box. It is looping through each custom
+select box and creating the divs that will be used to create the custom select box. */
+customSelects.forEach((customSelect) => {
+  const keepDefault = customSelect.hasAttribute("keepdefault");
+  const select = customSelect.querySelector("select");
+  const options = Array.from(select.options);
+  const divSelect = createDivElement(
+    options[select.selectedIndex].innerHTML,
+    "select-selected"
+  );
+  customSelect.appendChild(divSelect);
+
+  const divOptions = createDivElement("", "select-items select-hide");
+  options.forEach((option, i) => {
+    if (!i && !keepDefault) return;
+
+    const divOption = createDivElement(option.innerHTML);
+
+    divOption.addEventListener("click", function () {
+      const optionSelectedIndex = options.findIndex(
+        (option) => option.innerHTML === this.innerHTML
+      );
+      select.selectedIndex = optionSelectedIndex;
+      divSelect.innerHTML = options[optionSelectedIndex].innerHTML;
+      options.forEach((option) => option.classList.remove("same-as-selected"));
+      this.classList.add("same-as-selected");
+    });
+
+    divOptions.appendChild(divOption);
+  });
+  customSelect.appendChild(divOptions);
+
+  divSelect.addEventListener("click", handlerClickSelect);
+});
+
+/**
+ * Create a div element with the given html and className, and return it.
+ * @param html - The HTML to be inserted into the div.
+ * @param [className] - The class name of the div element.
+ * @returns A div element with the class name and inner HTML set.
+ */
+function createDivElement(html, className = "") {
+  const div = document.createElement("div");
+  div.className = className;
+  div.innerHTML = html;
+  return div;
+}
+
+/**
+ * When the user clicks on the select box, close all other select boxes, then open the one that was
+ * clicked on.
+ * @param e - the event object
+ */
+function handlerClickSelect(e) {
+  e.stopPropagation();
+  closeAllSelect(e.target);
+  e.target.nextSibling.classList.toggle("select-hide");
+  e.target.classList.toggle("select-arrow-active");
+}
+
+/**
+ * If the clicked select is not the same as the selected select, remove the arrow-active class and add
+ * the hide class.
+ * @param  - The select element that was clicked.
+ */
+function closeAllSelect(selectClicked) {
+  const selectSelecteds = document.querySelectorAll(".select-selected");
+
+  selectSelecteds.forEach((selectSelected) => {
+    if (selectSelected !== selectClicked) {
+      selectSelected.classList.remove("select-arrow-active");
+      selectSelected.nextElementSibling.classList.add("select-hide");
+    }
+  });
+}
+
+/* If the user clicks anywhere outside the select box,
+  then close all select boxes: */
+document.addEventListener("click", closeAllSelect);
